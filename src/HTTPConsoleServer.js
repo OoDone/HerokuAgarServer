b12895290355c5fe7d0fa4056526e7e5348bc16c
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-11-12 13:19:00 -0500

    Update HTTPConsoleServer.js

--- src/HTTPConsoleServer.js
+++ src/HTTPConsoleServer.js
@@ -1,1 +1,29 @@
-
+function startHttpServer(req, res) {
+  if (req.url == "/loginfail") {
+            fs.readFile("./../loginfail.html", function (err, data2) {
+                if (err) {
+                    res.writehead(404);
+                    res.write(err);
+                    res.end();
+                } else {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(data2);
+                    res.end();
+                }
+            });
+         } else {
+            fs.readFile("./../console.html", function(error, data) {
+                if (error) {  
+                        res.writeHead(404);  
+                        res.write(error);  
+                        res.end();  
+                    } else {  
+                        res.writeHead(200, {  
+                            'Content-Type': 'text/html'  
+                        });  
+                        res.write(data);  
+                        res.end();  
+                    } 
+            });
+         }
+}
