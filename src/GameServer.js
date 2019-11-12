a927b383779eebe81950d5d126441f5797f42998
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-11-12 13:22:34 -0500

    Update GameServer.js

--- src/GameServer.js
+++ src/GameServer.js
@@ -199,33 +199,8 @@ GameServer.prototype.start = function () {
 
     // Start the server
      this.httpServer = http.createServer((req, res) => {
-         if (req.url == "/loginfail") {
-            fs.readFile("./../loginfail.html", function (err, data2) {
-                if (err) {
-                    res.writehead(404);
-                    res.write(err);
-                    res.end();
-                } else {
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.write(data2);
-                    res.end();
-                }
-            });
-         } else {
-            fs.readFile("./../console.html", function(error, data) {
-                if (error) {  
-                        res.writeHead(404);  
-                        res.write(error);  
-                        res.end();  
-                    } else {  
-                        res.writeHead(200, {  
-                            'Content-Type': 'text/html'  
-                        });  
-                        res.write(data);  
-                        res.end();  
-                    } 
-            });
-         }
+         var CServer = require('./HTTPConsoleServer');
+         CServer.startHttpServer(req, res);
     })
     var wsOptions = {
         server: this.httpServer,
